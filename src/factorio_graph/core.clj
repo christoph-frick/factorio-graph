a1110ddbd2cd7e2ddf8f2715d724a371e119998f
Christoph Frick <rid@ofnir.net>
2015-10-04 23:59:56 +0200

    use rhizome instead of loom as it allows custering (later used for the the techlevelss); extracted some functions

--- src/factorio_graph/core.clj
+++ src/factorio_graph/core.clj
@@ -1,22 +1,48 @@
 (ns factorio-graph.core
   (:require [factorio-graph.parser :as p]
-            [loom.graph :as g]
-            [loom.io :as gio])
+            [rhizome.viz :refer [view-graph]])
   (:gen-class))
+
+(defn parse-recursive 
+  [directory]
+  (eduction
+    (filter #(.isFile %)) 
+    (mapcat #(p/parse-transform (slurp %))) 
+    (file-seq (clojure.java.io/file directory))))
+
+(defn factorio-dir
+  [& dirs]
+  (let [root-dir (System/getenv "FACTORIO")]
+    (assert (not (nil? root-dir)) "Set the environment variable FACTORIO to the root of your installation")
+    (apply str root-dir dirs)))
+
+(def ^:const DATA "/data/base")
+(def ^:const TECHNOLOGY "/prototypes/technology")
+
+(defn transpose
+  [g]
+  (reduce 
+    (fn [m [k v]]
+      (reduce (fn [m c] 
+                (update m c (fnil #(conj % k) []))) 
+              m v))
+    (into {} (map (fn [[k v]] [k []])) g) 
+    g))
+
+(defn technology-graph
+  []
+  (->> (parse-recursive (factorio-dir DATA TECHNOLOGY))
+       (filter #(= (:type %) "technology"))  
+       (map (fn [{:keys [name prerequisites]}] [name prerequisites]))
+       (into {})
+       transpose))
+
+(defn view-technology-graph
+  [graph]
+  (view-graph 
+    (keys graph) graph
+    :node->descriptor (fn [n] {:label n})))
 
 (defn -main
   [& args]
-  (let [root (System/getenv "FACTORIO")]
-    (assert (not (nil? root)) "Set the environment variable FACTORIO to the root of your installation")
-    (let [technology-dir (str root "/data/base/prototypes/technology/")
-          parsed (eduction
-                   (filter #(.isFile %)) 
-                   (mapcat #(p/parse-transform (slurp %))) 
-                   (file-seq (clojure.java.io/file root)))
-          technology (into {}
-                           (comp
-                             (filter #(= (:type %) "technology"))  
-                             (map (juxt :name :prerequisites)))
-                           parsed)
-          graph (-> technology g/digraph g/transpose)]
-      (gio/view graph :node {:shape "none"}))))
+  (view-technology-graph (technology-graph)))
